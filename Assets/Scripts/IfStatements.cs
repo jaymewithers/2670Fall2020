6d61f5975b165ae7a2229c95d02bed8155041aad
Jayme Withers <10859571@my.uvu.edu>
2020-08-25 10:38:24 -0600

    Refresher / if statements

---
+++ Assets/Scripts/IfStatements.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    public bool canRun;
+    public string password;
+    public int number;
+    private void Start()
+    {
+        if (canRun)
+        {
+            Debug.Log("Running");
+        }
+    }
+
+
+    private void Update()
+    {
+        if (!canRun)
+        {
+            Debug.Log("Running");
+        }
+
+        Debug.Log(password == "OU812" ? "Correct" : "Incorrect");
+
+        Debug.Log(number >= 7 ? "Greater than 7" : "Less than 7");
+    }
+}
