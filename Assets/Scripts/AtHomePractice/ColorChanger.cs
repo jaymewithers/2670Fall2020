f678fb8d423e7ebab3231f108497a9a44c226f31
Jayme Withers <10859571@my.uvu.edu>
2020-08-28 20:34:01 -0600

    Color changer attempt to update

---
+++ Assets/Scripts/AtHomePractice/ColorChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Renderer obj;
+    
+    private void Start()
+    {
+        obj = GetComponent<Renderer>();
+        obj.material.color = Color.red;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.G))
+        {
+            obj.material.color = Color.green;
+        }
+
+        if (Input.GetKey(KeyCode.Y))
+        {
+            obj.material.color = Color.yellow;
+        }
+    }
+}
