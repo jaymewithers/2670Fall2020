cc2a7a4e70e4f8b2b4170d4cd7061ce4d59fde7a
Jayme Withers <10859571@my.uvu.edu>
2020-08-31 14:02:21 -0600

    character controller

---
+++ Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 movement;
+    public float gravity = 9.81f;
+    public float moveSpeed = 3f;
+    public float fastMoveSpeed;
+    public float jumpForce = 10f;
+    public int jumpCountMax = 2;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal") * moveSpeed;
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            movement.y = jumpForce;
+        }
+
+        if (controller.isGrounded)
+        {
+            movement.y = 0;
+        }
+        else
+        {
+            movement.y -= gravity;
+        }
+       
+     
+        controller.Move(movement * Time.deltaTime);
+    }
+}
