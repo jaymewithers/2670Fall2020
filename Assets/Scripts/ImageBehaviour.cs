a040b0358a83febd8b51b0eac4f377f49699ed93
Jayme Withers <10859571@my.uvu.edu>
2020-09-04 10:57:37 -0600

    health bar depletion

---
+++ Assets/Scripts/ImageBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageBehaviour : MonoBehaviour
+{
+    private Image imageObj;
+    public FloatData valueObj;
+
+    private void Start()
+    {
+        imageObj = GetComponent<Image>();
+    }
+    
+    void Update()
+    {
+        imageObj.fillAmount = valueObj.value;
+    }
+}
