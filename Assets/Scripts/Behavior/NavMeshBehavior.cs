d48e17a34a5d7cf71e270ee2c25bb7f57baace59
Jayme Withers <10859571@my.uvu.edu>
2020-09-06 18:08:48 -0600

    nav mesh behavior

---
+++ Assets/Scripts/Behavior/NavMeshBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshBehavior : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public float speed;
+    public Transform player;
+    private void Start()
+    {
+        GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+    }
+
+
+    private void Update()
+    {
+        agent.destination = player.position;
+    }
+}
