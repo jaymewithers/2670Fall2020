7c93c8c5c4739cbdbfebfe8dafa47a1d052ea318
Jayme Withers <10859571@my.uvu.edu>
2020-09-07 10:57:59 -0600

    rigid body / nav mesh

--- Assets/Scripts/Behavior/NavMeshBehavior.cs
+++ Assets/Scripts/Behavior/NavMeshBehavior.cs
@@ -4,9 +4,15 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class NavMeshBehavior : MonoBehaviour
 {
-    private NavMeshAgent agent;
+    private readonly NavMeshAgent agent;
     public float speed;
     public Transform player;
+
+    public NavMeshBehavior(NavMeshAgent agent)
+    {
+        this.agent = agent;
+    }
+
     private void Start()
     {
         GetComponent<NavMeshAgent>();
---
+++ Assets/Scripts/Behavior/RigidBodyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidBodyBehaviour : MonoBehaviour
+{
+   private Rigidbody rigidBodyObj;
+   public float force;
+   private Vector3 forceObj;
+
+   private void Start()
+   {
+       rigidBodyObj = GetComponent<Rigidbody>();
+   }
+
+   private void OnCollisionEnter(Collision other)
+   {
+       forceObj.y = force;
+       rigidBodyObj.AddForce(forceObj);
+   }
+}
