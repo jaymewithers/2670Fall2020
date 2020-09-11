b8bd00370029857a7de053f788c93e15fad829a3
Jayme Withers <10859571@my.uvu.edu>
2020-09-11 11:26:23 -0600

    death

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+   public GameObject gameObj;
+   public FloatData health;
+
+   private void Update()
+   {
+      if (health.value <= 0)
+      {
+         gameObj.SetActive(false);
+      }
+   }
+}
