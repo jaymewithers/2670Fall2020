46b15203bc841d2030ae245fa05b31c649d20f45
Jayme Withers <10859571@my.uvu.edu>
2020-09-17 10:27:10 -0600

    knockback (still needs work)

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    public GameObject player, enemy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = enemy.transform.position * 2;
+    }
+}
