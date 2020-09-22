45f7a06868c2909a5edb772555d7864a53de4126
Jayme Withers <10859571@my.uvu.edu>
2020-09-22 09:51:41 -0600

    save

--- Assets/Scripts/ApplyForce.cs
+++ Assets/Scripts/ApplyForce.cs
@@ -6,13 +6,13 @@ public class ApplyForce : MonoBehaviour
     private Rigidbody rBody;
     public float force = 30f;
     public Vector3Data playerRotation;
-    public GameObject player;
+    public GameObject obj;
 
     private void Start()
     {
         rBody = GetComponent<Rigidbody>();
         var forceDirection = new Vector3(force, 0, 0);
-        player.transform.Rotate(playerRotation.value);
+        obj.transform.Rotate(playerRotation.value);
         rBody.AddRelativeForce(forceDirection);
     }
 }
