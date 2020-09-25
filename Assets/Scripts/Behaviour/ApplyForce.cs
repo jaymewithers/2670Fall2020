fe07a1eb19e5d431a12162a8f0eba12f453fa38f
Jayme Withers <10859571@my.uvu.edu>
2020-09-25 09:59:20 -0600

    fixed shooting

--- Assets/Scripts/Behaviour/ApplyForce.cs
+++ Assets/Scripts/Behaviour/ApplyForce.cs
@@ -4,12 +4,12 @@
 public class ApplyForce : MonoBehaviour
 {
     private Rigidbody rBody;
-    public float force = 30f;
+    public float force = 500f;
 
     public void Start()
     {
         rBody = GetComponent<Rigidbody>();
         var forceDirection = new Vector3(force, 0, 0);
-        rBody.AddRelativeForce(forceDirection * force);
+        rBody.AddRelativeForce(forceDirection);
     }
 }
