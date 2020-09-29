25b8d0d00fecf182836730dcbdd4a2e84e21a21a
Jayme Withers <10859571@my.uvu.edu>
2020-09-29 10:27:00 -0600

    text with string data

---
+++ Assets/Scripts/nah/TextDisplayBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDisplayBehaviour : MonoBehaviour
+{
+    public Text textObj;
+    public StringData value;
+
+    public void TextToStringData()
+    {
+        textObj.text = value.value;
+    }
+}
