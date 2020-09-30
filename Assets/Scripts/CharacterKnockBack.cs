d931dabae45b1c0286837b3167b01700663f264e
Jayme Withers <10859571@my.uvu.edu>
2020-09-30 14:15:35 -0600

    knockback / little nav mesh

---
+++ Assets/Scripts/CharacterKnockBack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CharacterKnockBack : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 knockBackVector;
+    public float knockBackForce = 5f;
+    private float tempForce;
+    
+    private void Start()
+    {
+        tempForce = knockBackForce;
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        knockBackForce = tempForce;
+        while (knockBackForce > 0)
+        {
+            knockBackVector.x = knockBackForce * Time.deltaTime;
+            controller.Move(knockBackVector);
+            knockBackForce -= 0.1f;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
