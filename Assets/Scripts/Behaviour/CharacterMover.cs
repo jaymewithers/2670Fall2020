4cd02ab0dc8dd811a7382652557c57a1c6c85a1f
Jayme Withers <10859571@my.uvu.edu>
2020-10-08 08:57:37 -0600

    knockback / coroutines / fixed jump

--- Assets/Scripts/Behaviour/CharacterMover.cs
+++ Assets/Scripts/Behaviour/CharacterMover.cs
@@ -7,11 +7,12 @@ public class CharacterMover : MonoBehaviour
     private CharacterController controller;
     private Vector3 movement;
 
-    public float rotateSpeed = 120f, gravity = -9.81f, jumpForce = 30f;
+    public float rotateSpeed = 120f, gravity = -9.81f, jumpForce = 10f;
     private float yVar;
 
     public FloatData normalSpeed, fastSpeed;
     private FloatData moveSpeed;
+    private bool canMove = true;
 
     public IntData playerJumpCount;
     private int jumpCount;
@@ -20,60 +21,74 @@ public class CharacterMover : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         moveSpeed = normalSpeed;
-    }
-    
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            moveSpeed = fastSpeed;
-        }
-
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            moveSpeed = normalSpeed;
-        }
-        
-        var vInput = Input.GetAxis("Vertical") * moveSpeed.value;
-        movement.Set(vInput, yVar, 0);
-
-        var hInput = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
-        transform.Rotate(0, hInput, 0);
-
-        yVar += gravity * Time.deltaTime;
-
-        if (controller.isGrounded && movement.y < 0)
-        {
-            yVar = -1f;
-            jumpCount = 0;
-        }
-
-        if (Input.GetButtonDown("Jump") && jumpCount < playerJumpCount.value)
-        {
-            yVar = jumpForce;
-            jumpCount++;
-        }
-
-        movement = transform.TransformDirection(movement);
-        controller.Move(movement* Time.deltaTime);
+        StartCoroutine(Move());
     }
 
-    public float playerKnockBackForce = 10f;
-    private Vector3 knockBackMovement;
+    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
 
-    private IEnumerator KnockBack(ControllerColliderHit hit)
+    private IEnumerator Move()
     {
+        canMove = true;
+        while (canMove)
+        {
+            yield return wffu;
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                moveSpeed = fastSpeed;
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                moveSpeed = normalSpeed;
+            }
+
+            var vInput = Input.GetAxis("Vertical") * moveSpeed.value;
+            movement.Set(vInput, yVar, 0);
+
+            var hInput = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
+            transform.Rotate(0, hInput, 0);
+
+            yVar += gravity * Time.deltaTime;
+
+            if (controller.isGrounded && movement.y < 0)
+            {
+                yVar = -1f;
+                jumpCount = 0;
+            }
+
+            if (Input.GetButtonDown("Jump") && jumpCount < playerJumpCount.value)
+            {
+                yVar = jumpForce;
+                jumpCount++;
+            }
+
+            movement = transform.TransformDirection(movement);
+            controller.Move(movement * Time.deltaTime);
+        }
+    }
+
+    private IEnumerator KnockBack(ControllerColliderHit hit, Rigidbody body)
+    {
+        canMove = false;
         var i = 2f;
-        knockBackMovement = hit.collider.attachedRigidbody.velocity * (i * playerKnockBackForce);
+        movement = -hit.moveDirection;
+        movement.y = -1;
         while (i > 0)
         {
-            yield return new WaitForFixedUpdate();
+            yield return wffu;
             i -= 0.1f;
+            controller.Move((movement) * Time.deltaTime);
+            
+            var pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+            var forces = pushDir * pushPower;
+            body.AddForce(forces);
         }
+        movement = Vector3.zero;
+        StartCoroutine(Move());
     }
-
-    private Vector3 direction = Vector3.zero;
-    public float pushPower = 3f;
+    
+    public float pushPower = 10.0f;
+    //private CharacterController characterController;
     
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
@@ -89,11 +104,6 @@ public class CharacterMover : MonoBehaviour
             return;
         }
 
-        StartCoroutine(KnockBack(hit));
-        
-        var pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-        var forces = pushDir * pushPower;
-        body.AddRelativeForce(forces);
-        body.AddTorque(forces);
+        StartCoroutine(KnockBack(hit, body));
     }
 }
