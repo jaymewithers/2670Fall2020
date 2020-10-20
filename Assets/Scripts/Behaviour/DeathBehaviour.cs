c3e15e524bb6aa3c057367436367431bda57b165
Jayme Withers <10859571@my.uvu.edu>
2020-10-20 09:34:57 -0600

    fixed death and respawn

--- Assets/Scripts/Behaviour/DeathBehaviour.cs
+++ Assets/Scripts/Behaviour/DeathBehaviour.cs
@@ -1,19 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class DeathBehaviour : MonoBehaviour
 {
    public GameObject gameObj;
-   public FloatData health;
+   public FloatData health, lives;
    public Vector3Data spawnPoint;
 
    private void Update()
    {
       if (!(health.value <= 0)) return;
       gameObj.SetActive(false);
+      StartCoroutine(Respawn());
+   }
 
-      if (gameObj.name != "Player") return;
-      gameObj.transform.position = spawnPoint.value;
-      health.value = 1f;
-      gameObj.SetActive(true);
+   private IEnumerator Respawn()
+   {
+      if (gameObj.name == "Player" && lives.value > 0)
+      {
+         yield return new WaitForSeconds(3f);
+         gameObj.transform.position = spawnPoint.value;
+         health.value = 1f;
+         gameObj.SetActive(true);
+         StopCoroutine(Respawn());
+      }
+
+      if (gameObj.name != "Player" || !(lives.value <= 0)) yield break;
+      gameObj.SetActive(false);
+      print("Game Over");
+      StopCoroutine(Respawn());
    }
 }
