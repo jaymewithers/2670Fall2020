e175d0181a5dc24c8648a57e83b60c4f43bd7937
Jayme Withers <10859571@my.uvu.edu>
2020-10-22 09:46:24 -0600

    start of draggable behaviour
    help from anthony's github

---
+++ Assets/Scripts/DraggableBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DraggableBehaviour : MonoBehaviour
+{
+    private Vector3 offsetPosition, newPosition; 
+    private bool canDrag { get; set; }
+    private Camera cam;
+    
+    public bool draggable { get; set; }
+    public UnityEvent onDrag, onUp;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        draggable = true;
+    }
+
+    private IEnumerator OnMouseDown()
+    {
+        onDrag.Invoke();
+        offsetPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+        yield return new WaitForFixedUpdate();
+        canDrag = true;
+        while (canDrag)
+        {
+            yield return new WaitForFixedUpdate();
+            newPosition = cam.ScreenToWorldPoint(Input.mousePosition) + offsetPosition;
+            transform.position = newPosition;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        canDrag = false;
+        if (draggable)
+        {
+            onUp.Invoke();
+        }
+    }
+}
--- Assets/Scripts/ScriptableObjects/StringListData.cs
+++ Assets/Scripts/ScriptableObjects/StringListData.cs
@@ -5,26 +5,23 @@ using UnityEngine.UI;
 [CreateAssetMenu]
 public class StringListData : ScriptableObject
 {
-   public List<string> stringList;
-   
-   private string returnValue;
+  public List<string> stringList;
+  private string returnValue;
+  private int i;
 
-   private int i;
+  private void OnEnable()
+  {
+    i = 0;
+  }
 
-   private void OnEnable()
-   {
-      i = 0;
-   }
+  public void GetNextString()
+  {
+    returnValue = stringList[i];
+    i = (i + 1) % stringList.Count;
+  }
 
-   public void GetNextString()
-   {
-      returnValue = stringList[i];
-      i = (i + 1) % stringList.Count;
-   }
-
-   public void SetTextUIToValue(Text obj)
-   {
-      obj.text = returnValue;
-   }
-
+  public void SetTextUIToValue(Text obj)
+  {
+    obj.text = returnValue;
+  }
 }
