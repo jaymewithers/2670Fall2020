167f61b2feef9f88ea0fc29e9ea3e0bb56e69c76
Jayme Withers <10859571@my.uvu.edu>
2020-10-25 12:21:54 -0600

    player carrying objects
    kinda weird but works in a way

---
+++ Assets/Scripts/CarryBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CarryBehaviour : MonoBehaviour
+{
+    public bool canCarry;
+    public Transform obj, player;
+    public float offset = 1.5f;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name != "MovableObject") return;
+        canCarry = true;
+    }
+    
+    private void Update()
+    {
+        if (canCarry && Input.GetKeyDown(KeyCode.Z))
+        {
+            obj.position = player.position * offset;
+            obj.transform.parent = player.transform;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            obj.transform.parent = null;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canCarry = false;
+    }
+}
