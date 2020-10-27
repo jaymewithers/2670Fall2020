d707b926d50b738234394ce3d4c3b1400cda71df
Jayme Withers <10859571@my.uvu.edu>
2020-10-27 09:56:44 -0600

    Camera follow player

---
+++ Assets/Scripts/CamBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CamBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
