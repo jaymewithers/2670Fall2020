b14783a3c82a718936f0c3dc2cc4081e23710615
Jayme Withers <10859571@my.uvu.edu>
2020-11-03 10:12:31 -0700

    push pull work

--- Assets/Scripts/PushPullRBody.cs
+++ Assets/Scripts/PushPullRBody.cs
@@ -1,26 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PushPullRBody : MonoBehaviour
 {
-    public bool canPush;
+    private Rigidbody rBody;
+    public bool canPickup;
     public Transform player;
-    private Vector3 offsetPosition, newPosition;
-    public UnityEvent onPush, onLetGo;
+    public Vector3 offset;
 
-    private IEnumerator OnTriggerEnter(Collider other)
+    private void Start()
     {
-        onPush.Invoke();
-        offsetPosition = transform.position;
-        yield return new WaitForFixedUpdate();
-        canPush = true;
-        while (canPush)
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+        if (canPickup && Input.GetKeyDown(KeyCode.Z))
         {
-            yield return new WaitForFixedUpdate();
-            newPosition = player.position + offsetPosition;
-            transform.position = newPosition;
-            other.transform.rotation = Quaternion.Euler(0,0,0);
+            var transformObj = transform;
+            transformObj.position = player.position + offset;
+            transformObj.parent = player;
+            canPickup = false;
+        }
+
+        if (!canPickup && Input.GetKeyUp(KeyCode.Z))
+        {
+            var transformObj = transform;
+            transformObj.parent = null;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        offset = other.transform.position;
+        player = other.transform;
+        canPickup = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canPickup = false;
+    }
+
+
+    // public bool canPush;
+    // public Transform player;
+    // public Vector3 offsetPosition, newPosition;
+    // public UnityEvent onPush, onLetGo;
+    //
+    // private IEnumerator OnTriggerEnter(Collider other)
+    // {
+    //     onPush.Invoke();
+    //     offsetPosition = transform.position;
+    //     yield return new WaitForFixedUpdate();
+    //     canPush = true;
+    //     while (canPush)
+    //     {
+    //         yield return new WaitForFixedUpdate();
+    //         newPosition = player.position + offsetPosition;
+    //         transform.position = newPosition;
+    //         other.transform.rotation = Quaternion.Euler(0,0,0);
+    //     }
 }
