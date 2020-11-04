f7e678f4090e9d2224349148d07be0aa379df808
Jayme Withers <10859571@my.uvu.edu>
2020-11-04 14:19:19 -0700

    Animation and StateMachines

---
+++ Assets/Scripts/CharacterBrain.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterBrain : ScriptableObject
+{
+    public Vector3Data position;
+
+    public Vector3 Move(float speed)
+    {
+        var newValue = position.value * speed;
+        return newValue;
+    }
+
+    public void Change(Vector3Data data)
+    {
+        position = data;
+    }
+}
---
+++ Assets/Scripts/CharacterMoveStates.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMoveStates : MonoBehaviour
+{
+    public UnityEvent startEvent;
+    public float speed = 5;
+    public CharacterBrain brain;
+    
+    private CharacterController controller;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        startEvent.Invoke();
+    }
+    
+    private void Update()
+    {
+        var position = brain.Move(speed * Input.GetAxis("Horizontal"));
+        var newPosition = position * Time.deltaTime;
+        controller.Move(newPosition);
+    }
+}
