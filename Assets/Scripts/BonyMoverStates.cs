6e1f01256d4a60ea83860eb1840b5b5a1591cbf1
Jayme Withers <10859571@my.uvu.edu>
2020-11-06 15:57:44 -0700

    Bony - StateMachines / anims / scripted

---
+++ Assets/Scripts/BonyMoverStates.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BonyMoverStates : MonoBehaviour
+{
+    public UnityEvent idleEvent, walkEvent, walkBackEvent, jumpEvent;
+    private void Start()
+    {
+        idleEvent.Invoke();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            walkEvent.Invoke();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            walkBackEvent.Invoke();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Jump());
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            idleEvent.Invoke();
+        }
+    }
+
+    private IEnumerator Jump()
+    {
+        jumpEvent.Invoke();
+        yield return new WaitForSeconds(3);
+        idleEvent.Invoke();
+        StopCoroutine(Jump());
+    }
+}
