dbf2fb17ef385e74b7fd70d17b2125c57a6e8fa2
Jayme Withers <10859571@my.uvu.edu>
2020-11-17 08:58:54 -0700

    messing with substance materials in unity

---
+++ Assets/Materials/SubstanceMaterials/SubstanceMaterialControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SubstanceMaterialControl : MonoBehaviour
+{
+    public Substance.Game.Substance material;
+    public Substance.Game.SubstanceGraph materialGraph;
+
+    private void Start()
+    {
+        materialGraph.SetInputColor("blue", Color.blue);
+    }
+}
