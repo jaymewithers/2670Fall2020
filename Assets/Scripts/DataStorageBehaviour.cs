26ceac567f152b6ced417cc4c4d10d8074a63915
Jayme Withers <10859571@my.uvu.edu>
2020-12-09 14:16:45 -0700

    data storage hard coded

---
+++ Assets/Scripts/DataStorageBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DataStorageBehaviour : MonoBehaviour
+{
+    public DataStorage dataStorageObj;
+    public FloatData floatDataObj;
+    public GameAction gameActionObj;
+
+    private void Start()
+    {
+        //hard coding instead of events
+        floatDataObj.value = 10f;
+        dataStorageObj.GetListData();
+        gameActionObj.Raise();
+    }
+
+    private void OnApplicationQuit()
+    {
+        dataStorageObj.SetData(floatDataObj);
+        dataStorageObj.SetListData();
+    }
+}
