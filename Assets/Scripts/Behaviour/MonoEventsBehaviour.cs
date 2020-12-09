d21134ddff0047c92d47ed7f3c7b009205d5c6f8
Jayme Withers <10859571@my.uvu.edu>
2020-12-09 14:08:15 -0700

    data storage

--- Assets/Scripts/Behaviour/MonoEventsBehaviour.cs
+++ Assets/Scripts/Behaviour/MonoEventsBehaviour.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class MonoEventsBehaviour : MonoBehaviour
 {
-    public UnityEvent startEvent, onEnable, awakeEvent;
+    public UnityEvent startEvent, onEnable, onDisableEvent, awakeEvent, onDestroyEvent, applicationQuitEvent;
     public float holdTime = 0.001f;
 
     public bool repeatOnStart;
@@ -29,5 +30,20 @@ public class MonoEventsBehaviour : MonoBehaviour
     private void OnEnable()
     {
         onEnable.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        onDisableEvent.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        onDestroyEvent.Invoke();
+    }
+
+    private void OnApplicationQuit()
+    {
+        applicationQuitEvent.Invoke();
     }
 }
